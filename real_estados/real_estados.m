dd54563ac23475fd874f775522887657d0c33668
JuanBiancuzzo <jbiancuzzo@fi.uba.ar>
2026-06-04 17:12:33 -0300

    Agregando lo que hicimos en la practica 8

---
+++ real_estados/real_estados.m
@@ -0,0 +1,26 @@
+clc
+
+%% Modelo con sobrepico - Intento 1
+poli = [1 8.029 105.9];
+k = 3.206;
+
+%% Modelo discretizado
+polos = roots(poli);
+T = 0.02;
+A_d = [ 1 T ; -polos(1)*polos(2)*T  1-(polos(1)+polos(2))*T ];
+B_d = [ 0; k*T ];
+C_d = [ 1 0 ];
+D_d = 0;
+
+%% Diseño de la matrix L de Luenberger
+polos_d = exp([ -100 -100 ] * T);
+L = acker(A_d', C_d', polos_d)'
+
+%% Diseño de K
+polos_r = exp([-5-9.8i -5+9.8i]* T);
+K = acker(A_d, B_d, polos_r)
+
+K = [10 0.5];
+
+%% Diseño del feed-forward
+F = inv(C_d*(eye(2)-inv(A_d+B_d*K))*B_d)

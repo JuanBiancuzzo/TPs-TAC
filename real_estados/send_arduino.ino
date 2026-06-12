50f4bd34999d36ddc503b75dbc443cad2cfa76de
JuanBiancuzzo <jbiancuzzo@fi.uba.ar>
2026-06-12 11:58:50 -0300

    Mejorando la forma de enviar para no tener que pensarlo

--- real_estados/send_arduino.ino
+++ real_estados/send_arduino.ino
@@ -1,18 +1,16 @@
 #include "send_arduino.h"
 
+#define TAM_MEDICIONES sizeof(info_enviar_t)
+typedef union {
+  info_enviar_t info;
+  byte vec[TAM_MEDICIONES];
+} datos_t;
+
 void enviar_datos(info_enviar_t info){  
   Serial.write("abcd");
 
-  float mediciones[] = { 
-    info.accion_control,
-    info.referencia,
-    
-    info.theta_medido,
-    info.theta_estimado,
+  datos_t mediciones;
+  mediciones.info = info;
 
-    info.omega_medida,
-    info.omega_estimada,
-  };
-
-  Serial.write((byte*) mediciones, sizeof(mediciones));
+  Serial.write(mediciones.vec, TAM_MEDICIONES);
 }
